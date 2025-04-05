8807867a6e62236e3787496cdbc001faaad6d56e
Durga-velu <durgadevi134babu@gmail.com>
2025-04-05 23:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle=jobTitle;
 	
 	this.jobGreet=function(){
-	   console.log(`Hello, my name is ${this.name} , I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 
